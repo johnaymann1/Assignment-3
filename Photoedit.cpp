5c6196c68638f264693fbeaf01fc1c01c9bea93b
John <99084287+johnaymann1@users.noreply.github.com>
2022-04-20 03:31:22 +0200

    Updat

--- Photoedit.cpp
+++ Photoedit.cpp
@@ -2,8 +2,8 @@
 // Purpose: program that can put a filter on a greyscale image
 // Author:  John Ayman Naim Aziz
 // Id:20210107
-// Date:    7 April 2022
-// Version: 3.0
+// Date:    20 April 2022
+// Version: 4.0
 
 #include <iostream>
 #include <fstream>
@@ -25,6 +25,8 @@ void darkenligten ();
 void merge_image();
 void detect_image_edges();
 void flip();
+void enlarge_img();
+void shuffle();
 
 
 int main()
@@ -34,7 +36,8 @@ int main()
     int x=0;
 program:
   loadImage();
-cout<<"Please choose the filter you want to apply on the image: \n (1)-Invert colors \n (2)-Rotate image \n (3)-Merge \n (4)-Darken or lighten \n (5)-detect image edges \n (6)-Flip \n";
+invalid:
+cout<<"Please choose the filter you want to apply on the image: \n (1)-Invert colors \n (2)-Rotate image \n (3)-Merge \n (4)-Darken or lighten \n (5)-detect image edges \n (6)-Flip \n (7)Enlarge \n (8)Shuffle \n";
 cin>>filter;
 switch(filter){
 case 1:
@@ -55,6 +58,15 @@ case 5:
 case 6:
      flip();
      break;
+case 7:
+     enlarge_img();
+     break;
+case 8:
+     shuffle();
+     break;
+default:
+goto invalid;
+break;
 }
   saveImage();
 cout<<"Do you want to do another image ?\n";
@@ -71,7 +83,7 @@ else if(contin=='n'|| contin=='N'){
 else{
     x=1;
 }
-while(x=1){
+while(x==1){
     cout<<"PLEASE ENTER A VALID LETTER (y) OR (n) \n";
     goto contt;
 }
@@ -152,17 +164,21 @@ void darkenligten () {
     for(int i = 0; i<SIZE ; i++){
         for(int j = 0; j<SIZE ; j++){
           //making the picture ligter in a specefic range
-          if(n ==1)
+          if(n ==1){
               if(image[i][j]>=200)
               continue;
               else
               image[i][j]+=50;
+          }
           //making the picture darker in a specefic range
-          else if (n==2)
-            if(image[i][j]<=70)
+          else if (n==2){
+            if(image[i][j]<=70){
             continue;
-            else
+            }
+            else{
             image[i][j]-=60;
+            }
+      }
       }
     }
 }
@@ -200,4 +216,187 @@ enterf:
         }
     }
 
+}
+void enlarge_img(){
+    int choice;
+    cout << "please enter the part you want to enlarge \n (1) or (2) \n  (3) or (4) \n";
+    cin >> choice;
+    //We will store the pixels of the chosen part in a new array 
+    switch (choice) {
+        case 1:
+            for (int i = 0; i < SIZE / 2; i++) {
+                for (int j = 0; j < SIZE / 2; j++)
+                    new_image[i][j] = image[i][j];
+            }
+            break;
+        case 2:
+            for (int i = 0; i < SIZE / 2 ; i++) {
+                for (int j = 0; j < SIZE /2 ; j++)
+                    new_image[i][j] = image[i][j + (SIZE / 2)];
+            }
+            break;
+        case 3:
+            for (int i = 0; i < SIZE /2 ; i++) {
+                for (int j = 0; j < SIZE /2; j++)
+                    new_image[i][j] = image[i + (SIZE / 2)][j];
+            }
+            break;
+        case 4:
+            for (int i = 0; i < SIZE / 2; i++) {
+                for (int j = 0; j < SIZE / 2; j++)
+                    new_image[i][j] = image[i + (SIZE / 2)][j + (SIZE / 2)];
+            }
+            break;
+        default:
+            cout << "invalid input\n";
+            enlarge_img();
+    }
+    // This loop for storing every pixel in the enlarged part into 4 pixel in the new image
+    for (int i = 0; i < SIZE / 2; i ++) {
+        for (int j = 0; j < SIZE / 2; j ++) {
+            image[i*2][j*2]=image[i*2][j*2+1]=image[i*2+1][j*2]=image[i*2+1][j*2+1] = new_image[i][j];
+        }
+    }
+}
+void shuffle(){
+int order1,order2,order3,order4;
+cout<<"Please enter the order of the picture with a space between every digit: "<<endl;
+cin>>order1>>order2>>order3>>order4;
+
+
+    switch (order1) {
+        case 1:
+            for (int i = 0; i < SIZE / 2; i++) {
+                for (int j = 0; j < SIZE / 2; j++)
+                    new_image[i][j] = image[i][j];
+            }
+            break;
+        case 2:
+            for (int i = 0; i < SIZE / 2 ; i++) {
+                for (int j = 0; j < SIZE /2 ; j++)
+                    new_image[i][j] = image[i][j + (SIZE / 2)];
+            }
+            break;
+        case 3:
+            for (int i = 0; i < SIZE /2 ; i++) {
+                for (int j = 0; j < SIZE /2; j++)
+                    new_image[i][j] = image[i + (SIZE / 2)][j];
+            }
+            break;
+        case 4:
+            for (int i = 0; i < SIZE / 2; i++) {
+                for (int j = 0; j < SIZE / 2; j++)
+                    new_image[i][j] = image[i + (SIZE / 2)][j + (SIZE / 2)];
+            }
+            break;
+        default:
+            cout << "invalid input\n";
+            shuffle();
+            }
+
+
+
+    switch (order2) {
+        case 1:
+            for (int i = 0; i < SIZE / 2; i++) {
+                for (int j = 0; j < SIZE / 2; j++)
+                    new_image[i][j + (SIZE / 2)] = image[i][j];
+            }
+            break;
+        case 2:
+            for (int i = 0; i < SIZE / 2 ; i++) {
+                for (int j = 0; j < SIZE /2 ; j++)
+                    new_image[i][j + (SIZE / 2)] = image[i][j + (SIZE / 2)];
+            }
+            break;
+        case 3:
+            for (int i = 0; i < SIZE /2 ; i++) {
+                for (int j = 0; j < SIZE /2; j++)
+                    new_image[i][j + (SIZE / 2)] = image[i + (SIZE / 2)][j];
+            }
+            break;
+        case 4:
+            for (int i = 0; i < SIZE / 2; i++) {
+                for (int j = 0; j < SIZE / 2; j++)
+                    new_image[i][j + (SIZE / 2)] = image[i + (SIZE / 2)][j + (SIZE / 2)];
+            }
+            break;
+        default:
+            cout << "invalid input\n";
+            shuffle();
+
+}
+
+
+
+    switch (order3) {
+        case 1:
+            for (int i = 0; i < SIZE / 2; i++) {
+                for (int j = 0; j < SIZE / 2; j++)
+                    new_image[i + (SIZE / 2)][j] = image[i][j];
+            }
+            break;
+        case 2:
+            for (int i = 0; i < SIZE / 2 ; i++) {
+                for (int j = 0; j < SIZE /2 ; j++)
+                    new_image[i + (SIZE / 2)][j] = image[i][j + (SIZE / 2)];
+            }
+            break;
+        case 3:
+            for (int i = 0; i < SIZE /2 ; i++) {
+                for (int j = 0; j < SIZE /2; j++)
+                    new_image[i + (SIZE / 2)][j] = image[i + (SIZE / 2)][j];
+            }
+            break;
+        case 4:
+            for (int i = 0; i < SIZE / 2; i++) {
+                for (int j = 0; j < SIZE / 2; j++)
+                    new_image[i + (SIZE / 2)][j] = image[i + (SIZE / 2)][j + (SIZE / 2)];
+            }
+            break;
+        default:
+            cout << "invalid input\n";
+            shuffle();
+
+}
+
+
+
+    switch (order4) {
+        case 1:
+            for (int i = 0; i < SIZE / 2; i++) {
+                for (int j = 0; j < SIZE / 2; j++)
+                    new_image[i + (SIZE / 2)][j + (SIZE / 2)] = image[i][j];
+            }
+            break;
+        case 2:
+            for (int i = 0; i < SIZE / 2 ; i++) {
+                for (int j = 0; j < SIZE /2 ; j++)
+                    new_image[i + (SIZE / 2)][j + (SIZE / 2)] = image[i][j + (SIZE / 2)];
+            }
+            break;
+        case 3:
+            for (int i = 0; i < SIZE /2 ; i++) {
+                for (int j = 0; j < SIZE /2; j++)
+                    new_image[i + (SIZE / 2)][j + (SIZE / 2)] = image[i + (SIZE / 2)][j];
+            }
+            break;
+        case 4:
+            for (int i = 0; i < SIZE / 2; i++) {
+                for (int j = 0; j < SIZE / 2; j++)
+                    new_image[i + (SIZE / 2)][j + (SIZE / 2)] = image[i + (SIZE / 2)][j + (SIZE / 2)];
+            }
+            break;
+        default:
+            cout << "invalid input\n";
+            shuffle();
+
+}
+
+
+for (int k = 0; k < SIZE; k++) {
+        for (int d = 0; d < SIZE; d++) {
+            image[k][d]=new_image[k][d];
+        }
+    }
 }
