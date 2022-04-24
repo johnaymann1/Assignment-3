749eb60dc4e0e8cfc18d1805215a9e54e902dc14
John <99084287+johnaymann1@users.noreply.github.com>
2022-04-24 19:36:32 +0200

    Photoedit V4

--- Photoedit.cpp
+++ Photoedit.cpp
@@ -2,7 +2,7 @@
 // Purpose: program that can put a filter on a greyscale image
 // Author:  John Ayman Naim Aziz
 // Id:20210107
-// Date:    20 April 2022
+// Date:    7 April 2022
 // Version: 4.0
 
 #include <iostream>
@@ -27,6 +27,12 @@ void detect_image_edges();
 void flip();
 void enlarge_img();
 void shuffle();
+void bw();
+void mirror();
+void blur();
+void shrink();
+void whiten_background();
+
 
 
 int main()
@@ -37,7 +43,7 @@ int main()
 program:
   loadImage();
 invalid:
-cout<<"Please choose the filter you want to apply on the image: \n (1)-Invert colors \n (2)-Rotate image \n (3)-Merge \n (4)-Darken or lighten \n (5)-detect image edges \n (6)-Flip \n (7)Enlarge \n (8)Shuffle \n";
+cout<<"Please choose the filter you want to apply on the image: \n (1)-Invert colors \n (2)-Rotate image \n (3)-Merge \n (4)-Darken or lighten \n (5)-detect image edges \n (6)-Flip \n (7)Enlarge \n (8)Shuffle \n (9)BW \n (10)Mirror \n (11)Blur \n (12)Shrink \n";
 cin>>filter;
 switch(filter){
 case 1:
@@ -63,6 +69,18 @@ case 7:
      break;
 case 8:
      shuffle();
+     break;
+case 9:
+     bw();
+     break;
+case 10:
+     mirror();
+     break;
+case 11:
+     blur();
+     break;
+case 12:
+     shrink();
      break;
 default:
 goto invalid;
@@ -401,3 +419,135 @@ for (int k = 0; k < SIZE; k++) {
     }
 }
 
+void bw()
+{
+    long avg = 0;
+    for (int i=0 ; i < SIZE; i++){
+        for (int j = 0; j < SIZE; j++)
+            avg += image[i][j]; // calculates the avarege of pixels to be used in comparison
+    }
+    avg /= (256*256);
+    for (int i=0 ; i < SIZE; i++){
+        for (int j=0 ; j < SIZE; j++){
+            if (image[i][j] > avg)
+                image[i][j] = 255;// changes pixel to white if it is greater than avg
+            else
+                image[i][j] = 0; // and to black if it is under the avg
+        }
+    }
+}
+
+
+void mirror()
+{
+    int choice;
+    cout << "please enter the part you want to reflect\n1- Left\n2- Right\n3- Upper\n4- Lower\n";
+    cin >> choice;
+    if(choice == 1){
+        for (int i=0; i < SIZE ; i++){
+            for(int j=0; j<SIZE ; j++)
+                image[i][SIZE-j]=image[i][j];// stores the left part of photo in the right side
+        }
+    }
+    if(choice == 2){
+        for (int i=0; i < SIZE ; i++){
+            for(int j=0; j<SIZE ; j++)
+                image[i][j] = image[i][SIZE-j]; // store the right part of the photo in the left part
+        }
+    }
+    if(choice == 3){
+        for (int i=0; i < SIZE ; i++){
+            for(int j=0; j<SIZE ; j++)
+                image[SIZE-i][j]= image[i][j]; // store the upper pixels in the lower pixels
+        }
+    }
+    if(choice == 4){
+        for (int i=0; i < SIZE ; i++){
+            for(int j=0; j<SIZE ; j++)
+                image[i][j] = image[SIZE-i][j]; // store the lower pixels of the photo in the upper part of the screen
+        }
+    }
+    else{ // recursion occurs if the choice is not 1 || 2 || 3 || 4
+        cout << "invalid input please enter 1 or 2 or 3 or 4\n";
+        mirror();
+    }
+}
+
+
+
+void blur()
+{
+    // calculate the average of every 6 pixels and store it in each pixel of the six pixels to give blurred view
+    for (int i=0; i < SIZE ; i++){
+        for(int j=0; j<SIZE ; j++){
+            image[i][j] = image[i+1][j] = image[i][j+1] = image[i+1][j+1] = image[i][j+2] = image[i+1][j+2] = (image[i][j+2] + image[i+1][j+2] + image[i][j] + image[i+1][j] + image[i][j+1] + image[i+1][j+1])/6;
+        }
+    }
+}
+
+
+
+
+void whiten_background() // this function will be used in the shrink filter to give the white background
+{
+    for (int i =0; i<SIZE ; i++) {
+        for (int j = 0; j < SIZE ; j++){
+            image[i][j] = 255;
+        }
+    }
+}
+
+
+
+
+
+void shrink()
+{
+    int choice; // choose the option of resizing
+    unsigned char temp1[SIZE/2][SIZE/2], temp2[SIZE/4][SIZE/4], temp3[SIZE/3][SIZE/3]; // the temp arrays that the resized photo will be stored in
+    // Taking each 4 neighbouring pixels and store the avg in one pixel to reduce the size to 128*128
+    for (int i=0; i < SIZE/2 ; i++){
+        for(int j=0; j<SIZE/2 ; j++) {
+            temp1[i][j] = (image[i*2][j*2] + image[i*2][(j*2)+1] + image[(i*2)+1][j*2] + image[(i*2)+1][(j*2)+1])/4;
+        }
+    }
+    // 1/4 dimensions by taking 1/2 dimensions of the first temp array
+    for (int i=0; i < SIZE/4 ; i++){
+        for(int j=0; j< SIZE/4 ; j++) {
+            temp2[i][j] = (temp1[i*2][j*2] + temp1[i*2][(j*2)+1] + temp1[(i*2)+1][j*2] + temp1[(i*2)+1][(j*2)+1])/4;
+        }
+    }
+    // for the reduced to third we will take one row and one column from the main img and leave 2 rows and 2 columns
+    for (int i=0; i < SIZE/3 ; i++){
+        for(int j=0; j<SIZE/3 ; j++) {
+            temp3[i][j] = image[i*3][j*3];
+        }
+    }
+    whiten_background(); // make the main img be a white background
+    cout << "please enter type of shrink\n 1- 1/2 dimensions\n 2- 1/4 dimentions\n 3- 1/3 dimentions\n";
+    cin >> choice;
+    // store the choosen dimensions as an overlay on the white background
+    switch (choice) {
+        case 1:
+            for (int i = 0; i < SIZE / 2; i++) {
+                for (int j = 0; j < SIZE / 2; j++) {
+                    image[i][j] = temp1[i][j];
+                }
+            }
+            break;
+        case 2:
+            for (int i = 0; i < SIZE / 4; i++) {
+                for (int j = 0; j < SIZE / 4; j++) {
+                    image[i][j] = temp2[i][j];
+                }
+            }
+            break;
+        case 3:
+            for (int i = 0; i < SIZE / 3; i++) {
+                for (int j = 0; j < SIZE / 3; j++) {
+                    image[i][j] = temp3[i][j];
+                }
+            }
+            break;
+    }
+}
